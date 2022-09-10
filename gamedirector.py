62e8a80fbd8f83b04f39e362d90350c7324ccd09
HajunPark <phajun105@naver.com>
2022-09-10 16:44:10 +0900

    빙고 체크

--- gamedirector.py
+++ gamedirector.py
@@ -28,7 +28,7 @@ class GameDirector(GameObject):
 
     self.tmr = self.tmr + 1
 
-    if self.mode == 3 and self.tmr % 5 == 0:
+    if self.mode == 3 and self.tmr % 2 == 0:
       board_x = self.player1.getColumnInBoard()
       board_y = self.player1.getRowInBoard()
       if board_y < 5 and self.board.boardData[board_y+1][board_x] == 0:
@@ -36,11 +36,16 @@ class GameDirector(GameObject):
       else:
         if board_y != -1:
           self.board.boardData[board_y][board_x] = 1
-          self.changeToMode2()
+          if self.checkBingo(1, board_y, board_x):
+            self.mode = 5
+          elif self.checkDraw():
+            self.mode = 7
+          else:
+            self.changeToMode2()
         else:
           self.mode = 1
     
-    if self.mode == 4 and self.tmr % 5 == 0:
+    if self.mode == 4 and self.tmr % 2 == 0:
       board_x = self.player2.getColumnInBoard()
       board_y = self.player2.getRowInBoard()
       if board_y < 5 and self.board.boardData[board_y+1][board_x] == 0:
@@ -48,11 +53,16 @@ class GameDirector(GameObject):
       else:
         if board_y != -1:
           self.board.boardData[board_y][board_x] = 2
-          self.changeToMode1()
+          if self.checkBingo(2, board_y, board_x):
+            self.mode = 6
+          elif self.checkDraw():
+            self.mode = 7
+          else:
+            self.changeToMode1()
         else:
           self.mode = 2
     
-    if self.mode == 5 or self.mode == 6:
+    if self.mode >= 5 and self.mode <= 7:
       self.player1.setVisible(False)
       self.player2.setVisible(False)
 
@@ -74,3 +84,95 @@ class GameDirector(GameObject):
     self.player1.setVisible(False)
     self.player2.setVisible(True)
     self.player1.reset()
+  
+  def CountStoneUpDown(self, playerNum, row, column):
+    sum = 0
+
+    for i in range(row, -1, -1):
+      if self.board.boardData[i][column] == playerNum:
+        sum = sum + 1
+      else: break
+    for i in range(row+1, 6, 1):
+      if self.board.boardData[i][column] == playerNum:
+        sum = sum + 1
+      else: break
+    
+    return sum
+
+  def CountStoneLeftRight(self, playerNum, row, column):
+    sum = 0
+
+    for i in range(column, -1, -1):
+      if self.board.boardData[row][i] == playerNum:
+        sum = sum + 1
+      else: break
+    for i in range(column+1, 7, 1):
+      if self.board.boardData[row][i] == playerNum:
+        sum = sum + 1
+      else: break
+    
+    return sum
+  
+  def CountStoneDiagonal1(self, playerNum, row, column):
+    sum = 0
+
+    for i in range(7):
+      if row-i == 0 or column-i == 0: break
+      else:
+        if self.board.boardData[row-i][column-i] == playerNum:
+          sum = sum + 1
+        else: break
+    for i in range(1, 7):
+      if row+i == 6 or column+i == 7: break
+      else:
+        if self.board.boardData[row+i][column+i] == playerNum:
+          sum = sum + 1
+        else: break
+    
+    return sum
+  
+  def CountStoneDiagonal2(self, playerNum, row, column):
+    sum = 0
+
+    for i in range(7):
+      if row-i == 0 or column+i == 7: break
+      else:
+        if self.board.boardData[row-i][column+i] == playerNum:
+          sum = sum + 1
+        else: break
+    for i in range(1, 7):
+      if row+i == 6 or column-i == 0: break
+      else:
+        if self.board.boardData[row+i][column-i] == playerNum:
+          sum = sum + 1
+        else: break
+    
+    return sum
+  
+  def CountAllStone(self):
+    sum = 0
+
+    for i in range(7):
+      for j in range(6):
+        if self.board.boardData[j][i] == 1 or self.board.boardData[j][i] == 2:
+          sum = sum + 1
+    
+    return sum
+
+  def checkBingo(self, playerNum, row, column):
+    if self.CountStoneUpDown(playerNum, row, column) == 4:
+      return True
+    elif self.CountStoneLeftRight(playerNum, row, column) == 4:
+      return True
+    elif self.CountStoneDiagonal1(playerNum, row, column) == 4:
+      return True
+    elif self.CountStoneDiagonal2(playerNum, row, column) == 4:
+      return True
+    else:
+      return False
+
+  def checkDraw(self):
+    if self.CountAllStone() == 42:
+      return True
+    else:
+      return False
